fb031d21170e09d9f0601c3d7d2445babfbf9c67
Ika1009 <ilija.doncic@yahoo.com>
2023-04-26 14:24:50 +0200

    Napravljen ceo shell lesgooo (samo pozadina ne radi)

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,22 @@
-﻿namespace BallChamps;
+﻿using System.Windows.Input;
+
+namespace BallChamps;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+    public ICommand GoToLoginCommand { get; private set; }
+
+    public AppShell()
 	{
 		InitializeComponent();
-	}
+
+        GoToLoginCommand = new Command(GoToLogin);
+        logOutItem.Command = GoToLoginCommand;
+    }
+
+    private async void GoToLogin()
+    {
+        bool answer = await DisplayAlert("Logout", "Are you sure you want to logout?", "Yes", "No");
+        if (answer) await Shell.Current.GoToAsync("//Intro");
+    }
 }
