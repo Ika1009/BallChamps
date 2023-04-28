b74365aed4c51b7d87a9ca16dea6fb3254a6d538
Ika1009 <ilija.doncic@yahoo.com>
2023-04-28 11:47:02 +0200

    Shell i kurci

--- View/CourtsPage.xaml.cs
+++ View/CourtsPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class CourtsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new ViewProfilePage(), true);
+    }
 }
--- View/ViewProfilePage.xaml.cs
+++ View/ViewProfilePage.xaml.cs
@@ -6,4 +6,9 @@ public partial class ViewProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new WaitingListPage(), true);
+    }
 }
