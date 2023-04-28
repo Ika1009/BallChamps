756ac6f22924ac29cff9a71e481f18f88dcb7a11
Ika1009 <ilija.doncic@yahoo.com>
2023-04-28 13:52:42 +0200

    2 3 pagea

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,6 @@ public partial class App : Application
     public App()
 	{ 
 		InitializeComponent();
-        MainPage = new AppShell();
+        MainPage = new PaymentPage();
 	}
 }
---
+++ View/PaymentPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BallChamps.View;
+
+public partial class PaymentPage : ContentPage
+{
+	public PaymentPage()
+	{
+		InitializeComponent();
+	}
+}
