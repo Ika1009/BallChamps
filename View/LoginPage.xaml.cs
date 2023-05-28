c317f1767e4a73c54fcb2832af20789e2491273d
Ika1009 <ilija.doncic@yahoo.com>
2023-05-28 22:08:43 +0200

    Integrated Login API

--- View/LoginPage.xaml.cs
+++ View/LoginPage.xaml.cs
@@ -1,15 +1,29 @@
+using BallChamps.Services;
+using System.Net.Http;
+
 namespace BallChamps.View;
 
 public partial class LoginPage : ContentPage
 {
-	//SignUpPage signUpPage; // use only one instance of signUpPage for optimization
     public LoginPage()
     {
         InitializeComponent();
     }
     async void LoginButton_Clicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync("//Home/HomePage");
+    {
+        if (string.IsNullOrEmpty(usernameEntry.Text)) { await Shell.Current.DisplayAlert("Error", "You need to fill in the username", "OK"); return; }
+        if (string.IsNullOrEmpty(passwordEntry.Text)) { await Shell.Current.DisplayAlert("Error", "You need to fill in the password", "OK"); return; }
+
+        try
+        {
+            bool loggedin = await APIService.LoginAsync(usernameEntry.Text, passwordEntry.Text);
+            if (loggedin)
+                await Shell.Current.GoToAsync("//Home/HomePage");
+            else
+                throw new Exception("Something went wrong");
+        }
+        catch (Exception ex) { await Shell.Current.DisplayAlert("Error", ex.Message, "OK"); }
+
     }
     async void OnSignUpClicked(object sender, EventArgs args)
 	{
